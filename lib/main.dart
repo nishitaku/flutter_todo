9484dbbf79cb72aac7277c2afa8c4cfa9a466b25
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-12-31 12:04:18 +0900

    Rename: MyHomePage -> TodoListPage

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,21 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TodoListPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class TodoListPage extends StatefulWidget {
+  TodoListPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _TodoListPageState createState() => _TodoListPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _TodoListPageState extends State<TodoListPage> {
   int _counter = 0;
 
   void _incrementCounter() {
