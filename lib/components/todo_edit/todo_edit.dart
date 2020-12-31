2d6d8eeedf0f9af3cef4687ddc863ae0caa71593
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-12-31 12:45:27 +0900

    Todo編集画面の追加

--- lib/components/todo_edit/todo_edit.dart
+++ lib/components/todo_edit/todo_edit.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo/components/todo_edit/todo_edit.dart';
 import 'package:flutter_todo/models/Todo.dart';
 
-class TodoListPage extends StatefulWidget {
-  TodoListPage({Key key, this.title}) : super(key: key);
+class TodoEditPage extends StatefulWidget {
+  TodoEditPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _TodoListPageState createState() => _TodoListPageState();
+  _TodoEditPageState createState() => _TodoEditPageState();
 }
 
-class _TodoListPageState extends State<TodoListPage> {
-  _moveToEditView() => Navigator.push(
-      context,
-      MaterialPageRoute(
-          builder: (context) => TodoEditPage(
-                title: widget.title,
-              )));
-
+class _TodoEditPageState extends State<TodoEditPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,25 +18,18 @@ class _TodoListPageState extends State<TodoListPage> {
         title: Text(widget.title),
       ),
       body: ListView.builder(
-          itemCount: 10,
+          itemCount: 3,
           itemBuilder: (BuildContext context, int index) {
             Todo todo = Todo("タイトル $index", DateTime.now(), "タイトル $index のメモ");
             return Card(
                 child: ListTile(
-              onTap: () {
-                _moveToEditView();
-              },
+              onTap: () {},
               title: Text("${todo.title}"),
               subtitle: Text("${todo.note}"),
               trailing: Text("${todo.dueDate.toLocal().toString()}"),
               isThreeLine: true,
             ));
           }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
