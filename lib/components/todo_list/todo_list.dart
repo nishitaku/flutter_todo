241a80e0f57267d6c2529faa093369f76378855c
nishitaku <29690066+nishitaku@users.noreply.github.com>
2021-01-06 01:03:26 +0900

    FutureBuilderを使って一覧を非同期で取得

--- lib/components/todo_list/todo_list.dart
+++ lib/components/todo_list/todo_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_todo/components/todo_add/todo_add.dart';
 import 'package:flutter_todo/components/todo_edit/todo_edit.dart';
 import 'package:flutter_todo/models/Todo.dart';
+import 'package:flutter_todo/repositories/db/db_provider.dart';
 
 class TodoListPage extends StatefulWidget {
   TodoListPage({Key key, this.title}) : super(key: key);
@@ -25,21 +26,39 @@ class _TodoListPageState extends State<TodoListPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index) {
-            Todo todo = Todo.newTodo("タイトル $index", "タイトル $index のメモ");
-            return Card(
-                child: ListTile(
-              onTap: () {
-                _moveToEditView(todo);
-              },
-              title: Text("${todo.title}"),
-              subtitle: Text("${todo.note}"),
-              trailing: Text("${todo.dueDate.toLocal().toString()}"),
-              isThreeLine: true,
-            ));
-          }),
+      body: FutureBuilder(
+        future: DBProvider.db.getAllTodos(),
+        builder: (BuildContext context, AsyncSnapshot<List<Todo>> snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            print(snapshot.error.toString());
+            return Text(snapshot.error.toString());
+          }
+
+          if (!snapshot.hasData) {
+            return Text("No data.");
+          }
+
+          return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                Todo todo = snapshot.data[index];
+                return Card(
+                    child: ListTile(
+                  onTap: () {
+                    _moveToEditView(todo);
+                  },
+                  title: Text("${todo.title}"),
+                  subtitle: Text("${todo.note}"),
+                  trailing: Text("${todo.dueDate.toLocal().toString()}"),
+                  isThreeLine: true,
+                ));
+              });
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _moveToAddView();
