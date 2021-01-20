cb3af3686619eaf50e5ae633d7ed32c678129e78
nishitaku <29690066+nishitaku@users.noreply.github.com>
2021-01-20 23:44:44 +0900

    更新画面: 更新ボタンの実装

--- lib/components/todo_edit/todo_edit.dart
+++ lib/components/todo_edit/todo_edit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/models/Todo.dart';
+import 'package:flutter_todo/repositories/db/db_provider.dart';
 
 class TodoEditPage extends StatefulWidget {
   TodoEditPage({Key key, this.todo}) : super(key: key);
@@ -11,37 +12,60 @@ class TodoEditPage extends StatefulWidget {
 }
 
 class _TodoEditPageState extends State<TodoEditPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _title;
+  String _note;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("${widget.todo.title}"),
+          title: Text(widget.todo.title),
         ),
         body: Container(
             margin: const EdgeInsets.all(16.0),
             child: Form(
+                key: _formKey,
                 child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'タイトル'),
-                  initialValue: widget.todo.title,
-                ),
-                SizedBox(height: 16.0),
-                TextFormField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'メモ'),
-                  initialValue: widget.todo.note,
-                ),
-                Container(
-                  width: double.infinity,
-                  child: RaisedButton(
-                      child: const Text('更新',
-                          style: TextStyle(color: Colors.white)),
-                      color: Colors.blueAccent,
-                      onPressed: () {}),
-                )
-              ],
-            ))));
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), labelText: 'タイトル'),
+                      initialValue: widget.todo.title,
+                      onSaved: (String value) {
+                        this._title = value;
+                      },
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), labelText: 'メモ'),
+                      initialValue: widget.todo.note,
+                      onSaved: (String value) {
+                        this._note = value;
+                      },
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: RaisedButton(
+                          child: const Text('更新',
+                              style: TextStyle(color: Colors.white)),
+                          color: Colors.blueAccent,
+                          onPressed: () {
+                            _update(widget.todo);
+                            Navigator.of(context).pop();
+                          }),
+                    )
+                  ],
+                ))));
+  }
+
+  void _update(Todo todo) async {
+    this._formKey.currentState.save();
+    print("Submit title=${this._title} note=${this._note}");
+    todo.title = this._title;
+    todo.note = this._note;
+    await DBProvider.db.updateTodo(todo);
   }
 }
